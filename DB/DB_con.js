6bf9e6c97b4f07791ad7aaedfee286f342d85f54
DevrajPun <johndoe@example.com>
2024-07-09 18:42:50 +0530

    live db added

--- DB/DB_con.js
+++ DB/DB_con.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = () => {
   return mongoose
-    .connect(process.env.DB_URL_LOCAL)
+    .connect(process.env.DB_LIVE_URL)
     .then((data) => {
       console.log(`Mongodb connected with server:${data.connection.host}`);
     })
